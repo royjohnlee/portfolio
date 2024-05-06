c9bf01d25ea006a2ac6abc98a68d906e584c31a5
royjohnlee <royjohnlee42@gmail.com>
2024-05-06 14:42:16 -0700

    change count year

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -110,7 +110,7 @@ const Hero = () => {
           <div className="md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3 md:mt-0 md:flex-col md:justify-end md:text-end">
             <div className="mb-10">
               <h2 className="text-headingColor font-[700] text-[32px]">
-                <CountUp start={0} end={1} duration={2} suffix="+" />
+                <CountUp start={0} end={4} duration={2} suffix="+" />
               </h2>
               <h4 className="text-headingColor font-[600] text-[18px]">
                 Years of Experience
