43d4b2a48e2a94335e63e5b9366ff8111517d201
Roy John Lee <royjohnlee@Roys-MacBook-Pro.local>
2024-09-03 22:26:40 -0700

    Fix resume

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -37,6 +37,7 @@ const Hero = () => {
                 </button>
               </a>
               <a
+                // href="src/assets/res/res.pdf"
                 a href="/RoyAugResume.pdf" 
                 className="text-smallTextColor font-[600] text-[16px] border-b border-solid border-smallTextColor"
                 download
